e770c93da5232aeb20c0b4a106848aa4c380f4a0
9806828552 <mrmayanksoni.soni@gmail.com>
2024-09-29 21:14:30 +0530

    delete not required code

--- XCentiumCodeChallenge/Models/ImageRepo.cs
+++ XCentiumCodeChallenge/Models/ImageRepo.cs
@@ -1,11 +1,8 @@
 ﻿using HtmlAgilityPack;
-using System;
-using System.Net;
-using System.Xml;
 
 namespace XCentiumCodeChallenge.Models
 {
-    public class ImageRepo:IImageRepo
+    public class ImageRepo : IImageRepo
     {
         public List<Image> GetAllImages(string pageUrl)
         {
@@ -13,14 +10,14 @@ namespace XCentiumCodeChallenge.Models
 
             var document = new HtmlWeb().Load(pageUrl);
             //var urls = document.DocumentNode.Descendants("img")
-              //                              .Select(e => e.GetAttributeValue("src", null))
-                //                            .Where(s => !String.IsNullOrEmpty(s));
+            //                              .Select(e => e.GetAttributeValue("src", null))
+            //                            .Where(s => !String.IsNullOrEmpty(s));
 
             foreach (string imgPath in document.DocumentNode.Descendants("img")
                                             .Select(e => e.GetAttributeValue("src", null))
                                             .Where(s => !String.IsNullOrEmpty(s)))
             {
-                images.Add(new Image() { ImageUrl = pageUrl +"/"+ imgPath }); 
+                images.Add(new Image() { ImageUrl = pageUrl + "/" + imgPath });
             }
 
             return images;
